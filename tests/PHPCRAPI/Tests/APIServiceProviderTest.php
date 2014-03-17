ba5e0445df0cf6dd58c2ae9aee39a6eb5f93833d
Robin Bressan <robin@buddey.net>
2014-03-17 09:24:44 +0100

    Uncomment test

--- tests/PHPCRAPI/Tests/APIServiceProviderTest.php
+++ tests/PHPCRAPI/Tests/APIServiceProviderTest.php
@@ -11,143 +11,87 @@ class APIServiceProviderTest extends WebTestCase
 		return require_once __DIR__.'/../../app.php';
 	}
 
-	// public function testGetRequestOnRootUrlShoudlReturnRepositoryNames()
-	// {
-	// 	$client = $this->createClient();
- //    	$crawler = $client->request('GET', '/api/repositories');
- //    	$this->assertTrue($client->getResponse()->isOk());
+	public function testGetRequestOnRootUrlShoudlReturnRepositoryNames()
+	{
+		$client = $this->createClient();
+    	$crawler = $client->request('GET', '/api/repositories');
+    	$this->assertTrue($client->getResponse()->isOk());
 
- //    	$json = json_decode($client->getResponse()->getContent(), true);
+    	$json = json_decode($client->getResponse()->getContent(), true);
 
- //    	$repository = array(
- //    		'name'			=>	'repository_test',
- //    		'factoryName'	=>	'jackalope.jackrabbit'
- //    	);
+        $this->assertArrayHasKey('support', $json[0]);
+        $this->assertArrayHasKey('name', $json[0]);
+        $this->assertArrayHasKey('factoryName', $json[0]);
+	}
 
- //    	$this->assertCount(1, $json['repositories']);
- //    	$this->assertContains($repository, $json['repositories']);
-	// }
+	public function testGetRequestOnRepositoryUrlShoudlReturnRepositoryInfos()
+	{
+		$client = $this->createClient();
+    	$crawler = $client->request('GET', '/api/repositories/repository_test');
+    	$this->assertTrue($client->getResponse()->isOk());
 
-	// public function testGetRequestOnRepositoryUrlShoudlReturnRepositoryInfos()
-	// {
-	// 	$client = $this->createClient();
- //    	$crawler = $client->request('GET', '/api/repositories/repository_test');
- //    	$this->assertTrue($client->getResponse()->isOk());
+    	$json = json_decode($client->getResponse()->getContent(), true);
 
- //    	$json = json_decode($client->getResponse()->getContent(), true);
+    	$this->assertArrayHasKey('support', $json);
+        $this->assertArrayHasKey('name', $json);
+        $this->assertArrayHasKey('factoryName', $json);
+	}
 
- //    	$repository = array(
- //    		'name'			=>	'repository_test',
- //    		'factoryName'	=>	'jackalope.jackrabbit'
- //    	);
+	public function testGetRequestOnRepositoryWorkspacesUrlShoudlReturnWorkspaceNames()
+	{
+		$client = $this->createClient();
+    	$crawler = $client->request('GET', '/api/repositories/repository_test/workspaces');
+    	$this->assertTrue($client->getResponse()->isOk());
 
- //    	$this->assertCount(1, $json);
- //    	$this->assertContains($repository, $json);
-	// }
+    	$json = json_decode($client->getResponse()->getContent(), true);
 
-	// public function testGetRequestOnRepositoryWorkspacesUrlShoudlReturnWorkspaceNames()
-	// {
-	// 	$client = $this->createClient();
- //    	$crawler = $client->request('GET', '/api/repositories/repository_test/workspaces');
- //    	$this->assertTrue($client->getResponse()->isOk());
+    	$this->assertContains(array('name' => 'default'), $json);
+	}
 
- //    	$json = json_decode($client->getResponse()->getContent(), true);
+	public function testGetRequestOnRepositoryWorkspaceUrlShoudlReturnWorkspaceInfos()
+	{
+		$client = $this->createClient();
+    	$crawler = $client->request('GET', '/api/repositories/repository_test/workspaces/default');
+    	$this->assertTrue($client->getResponse()->isOk());
 
- //    	$this->assertContains(array('name' => 'default'), $json['workspaces']);
- //    	$this->assertArrayHasKey('support', $json);
-	// }
+    	$json = json_decode($client->getResponse()->getContent(), true);
 
-	// public function testGetRequestOnRepositoryWorkspaceUrlShoudlReturnWorkspaceInfos()
-	// {
-	// 	$client = $this->createClient();
- //    	$crawler = $client->request('GET', '/api/repositories/repository_test/workspaces/default');
- //    	$this->assertTrue($client->getResponse()->isOk());
+    	$this->assertEquals('default', $json['workspace']['name']);
+    	$this->assertArrayHasKey('support', $json);
+	}
 
- //    	$json = json_decode($client->getResponse()->getContent(), true);
+	public function testGetRequestOnRepositoryWorkspaceNodeUrlShoudlReturnNodeInfos()
+	{
+		$this->markTestSkipped();
+	}
 
- //    	$this->assertEquals('default', $json['workspace']['name']);
- //    	$this->assertArrayHasKey('support', $json);
-	// }
-
-	// public function testGetRequestOnRepositoryWorkspaceNodeUrlShoudlReturnNodeInfos()
-	// {
-	// 	$client = $this->createClient();
-
- //    	$crawler = $client->request('GET', '/api/repositories/repository_test/workspaces/default/nodes');
- //    	$this->assertTrue($client->getResponse()->isOk());
-
- //    	$json = json_decode($client->getResponse()->getContent(), true);
-
- //    	$this->assertArrayHasKey('support', $json);
- //    	$this->assertArrayHasKey('node', $json);
- //    	$this->assertArrayHasKey('name', $json['node']);
- //    	$this->assertArrayHasKey('path', $json['node']);
- //    	$this->assertArrayHasKey('repository', $json['node']);
- //    	$this->assertArrayHasKey('workspace', $json['node']);
- //    	$this->assertArrayHasKey('children', $json['node']);
- //    	$this->assertArrayHasKey('hasChildren', $json['node']);
- //    	$this->assertArrayHasKey('nodeProperties', $json['node']);
-	// }
-
- //    public function testGetRequestOnRepositoryWorkspaceUnknownNodeUrlShoudReturn404()
- //    {
- //        $client = $this->createClient();
-
- //        $name = uniqid('n');var_dump($name);
- //        $crawler = $client->request('GET', sprintf('/api/repositories/repository_test/workspaces/default/nodes/%s',$name));
- //        $this->assertEquals(404, $client->getResponse()->getStatusCode());
- //    }
-
-    // public function testPostRequestOnRepositoryWorkspaceNodeUrlShoudlCreateAChildNode()
-    // {
-    //     $client = $this->createClient();
-
-    //     $relPath = 'myTestNode';
-    //     $crawler = $client->request(
-    //         'POST',
-    //         '/api/repositories/repository_test/workspaces/default/nodes',
-    //         array('relPath' =>  $relPath)
-    //     );
-    //     $this->assertTrue($client->getResponse()->isOk());
-
-    //     $json = json_decode($client->getResponse()->getContent(), true);
-    //     $this->assertEquals($json, sprintf('Node %s added to %s', $relPath, '/'));
-    // }
-
-    public function testPutRequestOnRepositoryWorkspaceNodeUrlShoudlUpdateAChildNode()
+    public function testGetRequestOnRepositoryWorkspaceUnknownNodeUrlShoudReturn404()
     {
         $client = $this->createClient();
-        $relPath = 'myTestNode';
-        $newName = uniqid('n');
+
+        $name = uniqid('n');var_dump($name);
+        $crawler = $client->request('GET', sprintf('/api/repositories/repository_test/workspaces/default/nodes/%s',$name));
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function testPostRequestOnRepositoryWorkspaceNodeUrlShoudlCreateAChildNode()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testPostRequestOnRepositoryWorkspacesShoudlCreateAWorkspace()
+    {
+        $client = $this->createClient();
+
+        $name = uniqid('Wk');
         $crawler = $client->request(
-            'PUT',
-            sprintf('/api/repositories/repository_test/workspaces/default/nodes/%s',$relPath),
-            array(
-                'newName' =>  $newName,
-                'method'  =>  'rename'
-            )
+            'POST',
+            '/api/repositories/repository_test/workspaces',
+            array('name' =>  $name)
         );
-        $json = json_decode($client->getResponse()->getContent(), true);
-        var_dump($json);
         $this->assertTrue($client->getResponse()->isOk());
 
         $json = json_decode($client->getResponse()->getContent(), true);
-        $this->assertEquals($json, sprintf('Node %s renamed', $newName));
+        $this->assertEquals($json, sprintf('Workspace %s created', $name));
     }
-
-    // public function testPostRequestOnRepositoryWorkspacesShoudlCreateAWorkspace()
-    // {
-    //     $client = $this->createClient();
-
-    //     $name = uniqid('Wk');
-    //     $crawler = $client->request(
-    //         'POST',
-    //         '/api/repositories/repository_test/workspaces',
-    //         array('name' =>  $name)
-    //     );
-    //     $this->assertTrue($client->getResponse()->isOk());
-
-    //     $json = json_decode($client->getResponse()->getContent(), true);
-    //     $this->assertEquals($json, sprintf('Workspace %s created', $name));
-    // }
 }
